8ff6d09e4c506d5aecde2bb3991ceef4809d38b3
Harutune Davis <hdavis2100@ip-172-31-44-180.us-east-2.compute.internal>
2025-10-08 03:47:45 -0500

    Initial structure

---
+++ src/config.php
@@ -0,0 +1,13 @@
+<?php
+return [
+	  'db' => [
+		      'host'    => '127.0.0.1',
+		          'port'    => 3306,
+			      'name'    => 'sportsbet',
+			          'user'    => 'your_mysql_user',
+				      'pass'    => 'your_mysql_password',
+				          'charset' => 'utf8mb4',
+					    ],
+					      'odds_api_key' => '', // fill later
+];
+
